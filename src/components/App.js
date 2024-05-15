a125d34a94717534cf902f8bb368dd445bb4faee
Emmanuel <emmanuelokello294@gmail.com>
2024-05-15 18:00:01 +0300

    completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,28 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  const handleFindPetsClick = () => {
+    let url = "http://localhost:3001/pets";
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+    fetch(url)
+   .then((res) => res.json())
+   .then(setPets);
+  };
+  
+
+
+
+  const handleAdoptPet = (petId) => {
+    setPets(pets.map(pet => pet.id === petId? {...pet, isAdopted: true} : pet));
+  };
+
+
+  const handleChangeType = (newType) => {
+    setFilters({type: newType})
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +37,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onFindPetsClick={handleFindPetsClick}  onChangeType={handleChangeType}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
